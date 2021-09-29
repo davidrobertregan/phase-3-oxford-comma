e62391934b3853a527c2dc4360d3eedefa8b182a
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2021-09-29 08:50:25 -0500

    commit tree b5bb29bf29e6dc00ff9a053b94f44ebf6aacb2af

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,21 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array) 
+    if array.length > 3
+        last = array.last
+        array.pop
+        array.push "and #{last}"
+        array.join(", ")
+        # array.join(", ")
+    elsif array.length == 3
+        array.join(", ")
+    elsif array.length == 2
+        array.join(" and ")
+    else 
+        array.join
+    end
 end
+
+array = ["kiwi", "durian", "starfruit", "mangos", "dragon fruits"]
+
+oxford_comma array
